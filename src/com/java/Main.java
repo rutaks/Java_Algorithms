9f6486861ea547e68af8b4ddbf5d1c4c402b56f9
rutaks <rootsum.dev@gmail.com>
2019-08-17 13:06:21 +0200

    Slight Change To Main

--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	    //Import A Class and call its methods Here
         //eg: FindTheParityOutlier findTheParityOutlier = new FindTheParityOutlier();
         //    System.out.println(findTheParityOutlier.find(new int[]{2, 4, 0, 100, 4, 11, 2602, 36}));
+        //Test
     }
 }
